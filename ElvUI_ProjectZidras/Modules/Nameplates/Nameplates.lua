d147d23ca484313d8a5584bf81169175b44e1a12
Zidras <10605951+Zidras@users.noreply.github.com>
2022-08-21 19:39:34 +0100

    NP: split tags from HD requirement + code optimization (#15)
    * NP: split tags from HD nameplates + avoid reload when (de)activating HD nameplates
    
    * NP: revert to reload required on HD client + code optimization
    
    Reduced a lot of unnecessary hooks (less intrusive on the NP overwriting) + implement event based checking for target/mouseover nameplate detection for better performance.

--- ElvUI_ProjectZidras/Modules/Nameplates/Nameplates.lua
+++ ElvUI_ProjectZidras/Modules/Nameplates/Nameplates.lua
@@ -12,8 +12,12 @@ local UnitName = UnitName
 
 local UnitIterator = T.UnitIterator
 
+local hasTarget = false
+
 -- GLOBALS: hooksecurefunc
 
+-- ElvUI Bugfixes -- (Doesn't need db check as they're intended to fix ElvUI core bugs or bad behaviour)
+-- Replace ElvUI Nameplate cache functions with proper GUID caching. .
 function NP:CacheArenaUnits()
 	twipe(self.ENEMY_PLAYER)
 	twipe(self.ENEMY_NPC)
@@ -161,6 +165,14 @@ function ZNP:CacheBossUnits()
 	end
 end
 
+-- Exiting on unit - self.unit mismatch would prevent castbars from showing on the nameplate that have unit already cached, so fix that too
+function NP:OnEvent(event, unit, ...)
+	if not unit then return end
+	NP:Update_CastBar(self, event, unit, ...) -- overwritten later on HD Nameplates
+end
+-- End ElvUI Bugfixes --
+
+-- For HD nameplates castbar spellName completion. Could also be used to generate castbars for non-HD clients if nameplateGUID is cached, but I won't develop it since HD nameplates are the better option
 function ZNP:COMBAT_LOG_EVENT_UNFILTERED(_, _, event, sourceGUID, sourceName, _, _, _, _, _, spellName)
 	if event == "SPELL_CAST_START" then
 		if sourceName and sourceGUID then -- sourceName needed, because in some cases there will be no sourceName, but only wrong unitguid so we skip those
@@ -172,236 +184,72 @@ function ZNP:COMBAT_LOG_EVENT_UNFILTERED(_, _, event, sourceGUID, sourceName, _,
 	end
 end
 
-local function updatePVPNameHook(self, frame)
-	ZNP.hooks[NP].Update_Name(self, frame)
+-- Below are the functions meant to be hooked if HD client is used
+local function OnCreatedHook(self, frame) -- this function requires a reload to properly replace the original function, since there are HookScripts in place that can't be unhooked (couldn't fully validate this)
+--	ZNP.hooks[NP].OnCreated(self, frame)
 
-	if not frame.unit then return end -- if Name Only, de-targeting will run UpdateAllFrame(frame, nil, true) and cause PVPName to reset to UnitName since frame.unit is nil
+	local CastBar = frame.UnitFrame.oldCastBar
+--	CastBar.Icon:SetParent(E.HiddenFrame) -- Hide the original castbar icon. Commented out and moved to OnValueChanged :Hide()
 
-	if UnitIsPlayer(frame.unit) then
-		frame.Name:SetText(UnitPVPName(frame.unit))
+	CastBar:HookScript("OnShow", ZNP.Update_CastBarOnShow)
+	CastBar:HookScript("OnHide", ZNP.Update_CastBarOnHide)
+	CastBar:HookScript("OnValueChanged", ZNP.Update_CastBarOnValueChanged)
+
+	frame.UnitFrame.CastBar:SetScript("OnUpdate", nil)
+end
+
+local function RegisterEventsHook(self, frame)
+	if not frame.unit then return end
+
+	if NP.db.units[frame.UnitType].health.enable or (frame.isTarget and NP.db.alwaysShowTargetHealth) then
+		if NP.db.units[frame.UnitType].castbar.enable then
+			frame:RegisterEvent("UNIT_SPELLCAST_INTERRUPTED")
+			frame:RegisterEvent("UNIT_SPELLCAST_CHANNEL_START")
+			frame:RegisterEvent("UNIT_SPELLCAST_FAILED")
+			frame.isEventsRegistered = true
+		end
+
+		NP.OnEvent(frame, nil, frame.unit)
 	end
 end
 
-function ZNP:Initialize()
-	if E.db.pz.nameplates.tags.title.enable then
-		if not ZNP:IsHooked(NP, "Update_Name") then
-			ZNP:RawHook(NP, "Update_Name", updatePVPNameHook, true)
+local function UnregisterAllEventsHook(self, frame)
+	frame:UnregisterEvent("UNIT_SPELLCAST_INTERRUPTED")
+	frame:UnregisterEvent("UNIT_SPELLCAST_CHANNEL_START")
+	frame:UnregisterEvent("UNIT_SPELLCAST_FAILED")
+	frame.isEventsRegistered = nil
+end
+
+local function UpdateCVarsHook(self)
+	SetCVar("ShowClassColorInNameplate", "1")
+	SetCVar("showVKeyCastbar", "1")
+	SetCVar("nameplateAllowOverlap", self.db.motionType == "STACKED" and "0" or "1")
+end
+
+function ZNP:CastBarHD()
+	if E.db.pz.nameplates.hdClient.hdNameplates then
+--[[ initially designed with AceHook to prevent not having to reload, but it caused unstable behavior (possibly due to hookscripts not being able to be unhooked), so revert back to a reload based toggle and hooksecurefunc/replace.
+		if not self:IsHooked(NP, "Update_CastBar") then
+			self:RawHook(NP, "Update_CastBar", ZNP.Update_CastBar) -- keep this before OnCreated hook
 		end
-	elseif not E.db.pz.nameplates.tags.title.enable then
-		if ZNP:IsHooked(NP, "Update_Name") then
-			ZNP:Unhook(NP, "Update_Name")
+		if not self:IsHooked(NP, "OnCreated") then
+			self:RawHook(NP, "OnCreated", OnCreatedHook, true)
 		end
-	end
-
-	if not E.db.pz.nameplates.hdClient.hdNameplates then return end -- Tags theoretically don't need HD client/nameplates, but since HD nameplates overwrite NP functions, refactoring this would be too high effort for low reward.
-
-	NP.Update_CastBar = ZNP.Update_CastBar
-
-	hooksecurefunc(NP, "OnCreated", function(this, frame)
-		local CastBar = frame.UnitFrame.oldCastBar
---		CastBar.Icon:SetParent(E.HiddenFrame) -- Hide the original castbar icon. Commented out and moved to OnValueChanged :Hide()
-
-		CastBar:HookScript("OnShow", ZNP.Update_CastBarOnShow)
-		CastBar:HookScript("OnHide", ZNP.Update_CastBarOnHide)
-		CastBar:HookScript("OnValueChanged", ZNP.Update_CastBarOnValueChanged)
-
-		frame.UnitFrame.CastBar:SetScript("OnUpdate", nil)
-	end)
-
-	hooksecurefunc(NP, "OnShow", function(np)
-		local frame = np.UnitFrame
-		if not frame.tagGUID then
-			frame.tagGUID = ZNP:Construct_tagGUID(frame) -- this is running here and not OnCreated due to a bug on ElvUI funtion runtime: UpdateElement_All finishes before OnShow, which finishes before OnCreated, so hooking them both is not possible unless I'd full replace OnCreated
+		if not self:IsHooked(NP, "RegisterEvents") then
+			self:RawHook(NP, "RegisterEvents", RegisterEventsHook)
 		end
-		ZNP:Configure_tagGUID(frame)
-		ZNP:Update_tagGUID(frame)
-	end)
-
-	hooksecurefunc(NP, "OnHide", function(np)
-		local frame = np.UnitFrame
-		if frame.tagGUID then
-			frame.tagGUID:SetText()
+		if not self:IsHooked(NP, "UnregisterAllEvents") then
+			self:RawHook(NP, "UnregisterAllEvents", UnregisterAllEventsHook)
 		end
-	end)
-
-	NP.OnEvent = function(this, event, unit, ...)
-		if not unit then return end
-		NP:Update_CastBar(this, event, unit, ...)
-	end
-
-	NP.RegisterEvents = function(this, frame)
-		if not frame.unit then return end
-
-		if NP.db.units[frame.UnitType].health.enable or (frame.isTarget and NP.db.alwaysShowTargetHealth) then
-			if NP.db.units[frame.UnitType].castbar.enable then
-				frame:RegisterEvent("UNIT_SPELLCAST_INTERRUPTED")
-				frame:RegisterEvent("UNIT_SPELLCAST_CHANNEL_START")
-				frame:RegisterEvent("UNIT_SPELLCAST_FAILED")
-				frame.isEventsRegistered = true
-			end
-
-			NP.OnEvent(frame, nil, frame.unit)
+		if not self:IsHooked(NP, "UpdateCVars") then
+			self:RawHook(NP, "UpdateCVars", UpdateCVarsHook)
 		end
-	end
-
-	NP.UnregisterAllEvents = function(this, frame)
-		frame:UnregisterEvent("UNIT_SPELLCAST_INTERRUPTED")
-		frame:UnregisterEvent("UNIT_SPELLCAST_CHANNEL_START")
-		frame:UnregisterEvent("UNIT_SPELLCAST_FAILED")
-		frame.isEventsRegistered = nil
-	end
-
-	-- Couldn't find an efficient workaround to the UpdateElement_All finishing before OnShow/OnCreated, and hooking SetTargetFrame is onUpdate, so I am overwriting NP SetTargetFrame instead, to properly place the Update_tagGUID so it only runs once
-	local hasTarget
-	hooksecurefunc(NP, "PLAYER_TARGET_CHANGED", function()
-		hasTarget = UnitExists("target") == 1
-	end)
-	NP.SetTargetFrame = function(this, frame)
-		if hasTarget and frame.alpha == 1 then
-			if not frame.isTarget then
-				frame.isTarget = true
-
-				this:SetPlateFrameLevel(frame, this:GetPlateFrameLevel(frame), true)
-
-				if this.db.useTargetScale then
-					this:SetFrameScale(frame, (frame.ThreatScale or 1) * this.db.targetScale)
-				end
-
-				if not frame.isGroupUnit then
-					frame.unit = "target"
-					frame.guid = UnitGUID("target")
-					ZNP:Update_tagGUID(frame)
-
-					if E.db.pz.nameplates.tags.title.enable then
-						NP:Update_Name(frame)
-					end
-
-					this:RegisterEvents(frame)
-				end
-
-				this:UpdateElement_Auras(frame)
-
-				if not this.db.units[frame.UnitType].health.enable and this.db.alwaysShowTargetHealth then
-					frame.Health.r, frame.Health.g, frame.Health.b = nil, nil, nil
-
-					this:Configure_HealthBar(frame)
-					this:Configure_CastBar(frame)
-					this:Configure_Elite(frame)
-					this:Configure_CPoints(frame)
-
-					this:RegisterEvents(frame)
-
-					this:UpdateElement_All(frame, true)
-				end
-
-				NP:PlateFade(frame, NP.db.fadeIn and 1 or 0, frame:GetAlpha(), 1)
-
-				this:Update_Highlight(frame)
-				this:Update_CPoints(frame)
-				this:StyleFilterUpdate(frame, "PLAYER_TARGET_CHANGED")
-				this:ForEachVisiblePlate("ResetNameplateFrameLevel") --keep this after `StyleFilterUpdate`
-			end
-		elseif frame.isTarget then
-			frame.isTarget = nil
-
-			this:SetPlateFrameLevel(frame, this:GetPlateFrameLevel(frame))
-
-			if this.db.useTargetScale then
-				this:SetFrameScale(frame, (frame.ThreatScale or 1))
-			end
-
-			if not frame.isGroupUnit then
-				frame.unit = nil
-
-				if frame.isEventsRegistered then
-					this:UnregisterAllEvents(frame)
-					this:Update_CastBar(frame)
-				end
-			end
-
-			if not this.db.units[frame.UnitType].health.enable then
-				this:UpdateAllFrame(frame, nil, true)
-			end
-
-			this:Update_CPoints(frame)
-
-			if not frame.AlphaChanged then
-				if hasTarget then
-					NP:PlateFade(frame, NP.db.fadeIn and 1 or 0, frame:GetAlpha(), this.db.nonTargetTransparency)
-				else
-					NP:PlateFade(frame, NP.db.fadeIn and 1 or 0, frame:GetAlpha(), 1)
-				end
-			end
-
-			this:StyleFilterUpdate(frame, "PLAYER_TARGET_CHANGED")
-			this:ForEachVisiblePlate("ResetNameplateFrameLevel") --keep this after `StyleFilterUpdate`
-		else
-			if hasTarget and not frame.isAlphaChanged then
-				frame.isAlphaChanged = true
-
-				if not frame.AlphaChanged then
-					NP:PlateFade(frame, NP.db.fadeIn and 1 or 0, frame:GetAlpha(), this.db.nonTargetTransparency)
-				end
-
-				this:StyleFilterUpdate(frame, "PLAYER_TARGET_CHANGED")
-			elseif not hasTarget and frame.isAlphaChanged then
-				frame.isAlphaChanged = nil
-
-				if not frame.AlphaChanged then
-					NP:PlateFade(frame, NP.db.fadeIn and 1 or 0, frame:GetAlpha(), 1)
-				end
-
-				this:StyleFilterUpdate(frame, "PLAYER_TARGET_CHANGED")
-			end
-		end
-
-		this:Configure_Glow(frame)
-		this:Update_Glow(frame)
-	end
-
-	NP.SetMouseoverFrame = function(this, frame)
-		if frame.oldHighlight:IsShown() then
-			if not frame.isMouseover then
-				frame.isMouseover = true
-
-				this:Update_Highlight(frame)
-
-				if not frame.isGroupUnit then
-					frame.unit = "mouseover"
-					frame.guid = UnitGUID("mouseover")
-					ZNP:Update_tagGUID(frame)
-					if E.db.pz.nameplates.tags.title.enable then
-						NP:Update_Name(frame)
-					end
-
-					NP:Update_CastBar(frame, nil, frame.unit)
-				end
-
-				this:UpdateElement_Auras(frame)
-			end
-		elseif frame.isMouseover then
-			frame.isMouseover = nil
-
-			this:Update_Highlight(frame)
-
-			if not frame.isGroupUnit then
-				frame.unit = nil
-
-				NP:Update_CastBar(frame)
-			end
-		end
-
-		this:StyleFilterUpdate(frame, "UNIT_AURA")
-	end
-
-	NP.UpdateCVars = function(this)
-		SetCVar("ShowClassColorInNameplate", "1")
-		SetCVar("showVKeyCastbar", "1")
-		SetCVar("nameplateAllowOverlap", NP.db.motionType == "STACKED" and "0" or "1")
-	end
-
-	if NP.Initialized then
-		NP:UpdateCVars()
+]]
+		NP.Update_CastBar = ZNP.Update_CastBar -- keep this before OnCreated hook
+		hooksecurefunc(NP, "OnCreated", OnCreatedHook)
+		NP.RegisterEvents = RegisterEventsHook
+		NP.UnregisterAllEvents = UnregisterAllEventsHook
+		NP.UpdateCVars = UpdateCVarsHook
 
 		for frame in pairs(NP.CreatedPlates) do
 			frame:UnregisterEvent("UNIT_SPELLCAST_DELAYED")
@@ -414,9 +262,153 @@ function ZNP:Initialize()
 
 			frame.UnitFrame.CastBar:SetScript("OnUpdate", nil)
 		end
+--[[elseif not E.db.pz.nameplates.hdClient.hdNameplates then
+		if self:IsHooked(NP, "Update_CastBar") then
+			self:Unhook(NP, "Update_CastBar")
+		end
+		if self:IsHooked(NP, "OnCreated") then
+			self:Unhook(NP, "OnCreated")
+		end
+		if self:IsHooked(NP, "RegisterEvents") then
+			self:Unhook(NP, "RegisterEvents")
+		end
+		if self:IsHooked(NP, "UnregisterAllEvents") then
+			self:Unhook(NP, "UnregisterAllEvents")
+		end
+		if self:IsHooked(NP, "UpdateCVars") then
+			self:Unhook(NP, "UpdateCVars")
+		end
+		for frame in pairs(NP.CreatedPlates) do
+			frame:RegisterEvent("UNIT_SPELLCAST_DELAYED")
+			frame:RegisterEvent("UNIT_SPELLCAST_CHANNEL_UPDATE")
+			frame:RegisterEvent("UNIT_SPELLCAST_CHANNEL_STOP")
+			frame:RegisterEvent("UNIT_SPELLCAST_INTERRUPTIBLE")
+			frame:RegisterEvent("UNIT_SPELLCAST_NOT_INTERRUPTIBLE")
+			frame:RegisterEvent("UNIT_SPELLCAST_START")
+			frame:RegisterEvent("UNIT_SPELLCAST_STOP")
+
+			frame.UnitFrame.CastBar:SetScript("OnUpdate", NP.Update_CastBarOnUpdate)
+		end]]
+	end
+end
+
+-- Tags
+function ZNP:Update_Tags(frame)
+	local tagsOptions = E.db.pz.nameplates.tags
+
+	if tagsOptions.guid.enable then
+		ZNP:Update_tagGUID(frame)
+	end
+	if tagsOptions.title.enable then
+		NP:Update_Name(frame)
+	end
+end
+
+function ZNP:PLAYER_TARGET_CHANGED()
+	hasTarget = UnitExists("target") == 1
+	if hasTarget then
+		E:Delay(0.01, function() -- Delay needed since nameplate alpha only gets set a few frames after the event fires
+			for frame in pairs(NP.VisiblePlates) do
+				if frame.alpha == 1 then
+					ZNP:Update_Tags(frame)
+					break
+				end
+			end
+		end)
+	end
+end
+
+function ZNP:UPDATE_MOUSEOVER_UNIT()
+	if UnitExists("mouseover") and not UnitIsUnit("mouseover", "player") then
+		for frame in pairs(NP.VisiblePlates) do
+			if frame.oldHighlight:IsShown() then
+				-- runs before SetMouseoverFrame so ensure frame.unit and frame.guid are the right values (also needed due to CURSOR_UPDATE when mouseovering a model and afterwards a different nameplate would assume a different frame.guid)
+				frame.unit = "mouseover"
+				frame.guid = UnitGUID("mouseover")
+
+				ZNP:Update_Tags(frame)
+				break
+			end
+		end
+	end
+end
+ZNP.CURSOR_UPDATE = ZNP.UPDATE_MOUSEOVER_UNIT
+
+local function OnShowHook(self)
+	local frame = self.UnitFrame
+	if not frame.tagGUID then
+		frame.tagGUID = ZNP:Construct_tagGUID(frame) -- this is running here and not OnCreated due to a bug on ElvUI funtion runtime: UpdateElement_All finishes before OnShow, which finishes before OnCreated, so hooking them both is not possible unless I'd full replace OnCreated
+	end
+	ZNP:Configure_tagGUID(frame)
+	ZNP:Update_Tags(frame)
+
+	if hasTarget then
+		E:Delay(0.01, function() if frame.alpha == 1 then ZNP:Update_Tags(frame) end end) -- Delay needed since nameplate alpha only gets set a few frames after this
+	end
+end
+
+local function OnHideHook(self)
+	local frame = self.UnitFrame
+	if frame.tagGUID then
+		frame.tagGUID:SetText()
+	end
+end
+
+local function updatePVPNameHook(self, frame)
+	if not frame.unit then return end -- if Name Only, de-targeting will run UpdateAllFrame(frame, nil, true) and cause PVPName to reset to UnitName since frame.unit is nil
+
+	if UnitIsPlayer(frame.unit) then
+		frame.Name:SetText(UnitPVPName(frame.unit))
+	end
+end
+
+function ZNP:NameplateTags()
+	local tagsOptions = E.db.pz.nameplates.tags
+	if tagsOptions.guid.enable or tagsOptions.title.enable then
+		if not self:IsHooked(NP, "OnShow") then
+			self:SecureHook(NP, "OnShow", OnShowHook)
+		end
+		if not self:IsHooked(NP, "OnHide") then
+			self:SecureHook(NP, "OnHide", OnHideHook)
+		end
+	elseif not tagsOptions.guid.enable and not tagsOptions.title.enable then
+		if self:IsHooked(NP, "OnShow") then
+			self:Unhook(NP, "OnShow")
+		end
+		if self:IsHooked(NP, "OnHide") then
+			NP:ForEachPlate("UpdateAllFrame", true, true) -- run it once before unhooking to clear the ZNP tags
+			self:Unhook(NP, "OnHide")
+		end
+	end
+	if tagsOptions.title.enable then
+		if not self:IsHooked(NP, "Update_Name") then
+			self:SecureHook(NP, "Update_Name", updatePVPNameHook)
+		end
+	elseif not tagsOptions.title.enable then
+		if self:IsHooked(NP, "Update_Name") then
+			self:Unhook(NP, "Update_Name")
+		end
+	end
+end
+
+function ZNP:UpdateAllSettings()
+	self:CastBarHD()
+	self:NameplateTags()
+end
+
+function ZNP:Initialize()
+	if NP.Initialized then
+		ZNP:UpdateAllSettings()
+
+		if E.db.pz.nameplates.hdClient.hdNameplates then
+			UpdateCVarsHook(NP) -- update once since we cannot hook it in time on NP:Initialize.
+		end
 	end
 
 	ZNP:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+	ZNP:RegisterEvent("PLAYER_TARGET_CHANGED")
+	ZNP:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
+	ZNP:RegisterEvent("CURSOR_UPDATE")
 
 	-- Bosses
 	ZNP:CacheBossUnits()
